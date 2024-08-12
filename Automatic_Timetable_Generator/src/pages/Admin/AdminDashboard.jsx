796dafae9c2803d087e186cd52ff7de93b0e0819
Vikram P <125629713+Vikram05vikky@users.noreply.github.com>
2024-08-12 10:20:33 +0530

    Delete Automatic_Timetable_Generator directory

--- Automatic_Timetable_Generator/src/pages/Admin/AdminDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AdminDashboard = () => {
-  return (
-    <div className='flex justify-center items-center '>AdminDashboard</div>
-  )
-}
-
-export default AdminDashboard
